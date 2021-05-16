517b12e856d33b41f5849c41db9724a4241371bd
Vlad Cornea <corneavlad98@yahoo.com>
2021-05-16 14:51:41 +0300

    Finished servo settings tab (with saving)

--- Licenta_PlanB/interface.py
+++ Licenta_PlanB/interface.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter.font import BOLD
+from tkinter import messagebox
 
 #Define some colors
 backgroundColor = '#92b8c5' # cyan-ish color
@@ -22,43 +23,73 @@ labelFont = ("Helvetica", 15 )
 titleLabelFont = ("Helvetica", 15, BOLD)
 detailsTextFont =("Helvetica", 14)
 
-def servo_settings_tab(tab):
-    # Title Label
-    titleLabel = Label(tab, text="EggBot: Basic Setup - Servo Motor ", font=titleLabelFont)
-    titleLabel.place(x=20,y=15)
+servoSettingsEntries = ['4', '90', '120']
 
-    padding_x = 45
-    padding_y = 25
-    # Labels
-    servoPinLabel = Label(tab, text="Servo pin number (GPIO numbering): ", font=labelFont)
-    servoPinLabel.place(x=50 + padding_x,y=65 + padding_y)
 
-    servoUpLabel = Label(tab, text="Pen up position, servo at (0-180): ", font=labelFont)
-    servoUpLabel.place(x=50 + padding_x,y=120 + padding_y)
 
-    servoDownLabel = Label(tab, text="Pen down position, servo at (0-180): ", font=labelFont)
-    servoDownLabel.place(x=50 + padding_x,y=175 + padding_y)
+class Servo_Settins_Tab():
+    def __init__(self, tab):
+        # Define some paddings
+        self.padding_x = 45
+        self.padding_y = 25
 
-    details = "-Raise and lower pen to check pen-up and pen-down positions"
-    detailsLabel = Label(tab, text=details, font=detailsTextFont)
-    detailsLabel.place(x=25 + padding_x, y=275 + padding_y)
+        # Title label
+        self.titleLabel = Label(tab, text="EggBot: Basic Setup - Servo Motor ", font=titleLabelFont)
+        self.titleLabel.place(x=20,y=15)
 
-    # Entry fields
-    servoPinEntry = Entry(tab, font=entryFieldFont)
-    servoPinEntry.insert(0, '4')
-    servoPinEntry.place(x=400 + padding_x,y=67 + padding_y, width=46, height=30)
-
-    servoUpEntry = Entry(tab, font=entryFieldFont)
-    servoUpEntry.insert(0, '105')
-    servoUpEntry.place(x=400 + padding_x,y=122 + padding_y, width=46, height=30)
-
-    servoDownEntry = Entry(tab, font=entryFieldFont)
-    servoDownEntry.insert(0, '140')
-    servoDownEntry.place(x=400 + padding_x,y=177 + padding_y, width=46, height=30)
+        self.add_labels(tab)
+        self.add_entries(tab)
+       
+        # Add save button to tab
+        saveButtonServo = Button(tab, text="Save", font = saveButtonFont, width = 5, fg = 'black', command=self.update)
+        saveButtonServo.place(x=290,y=420)
     
-    # Add save button
-    saveButton = Button(tab, text="Save", font = saveButtonFont, width = 5, fg = 'black')
-    saveButton.place(x=290,y=420)
+    def add_labels(self, tab):
+        # Labels
+        self.servoPinLabel = Label(tab, text="Servo pin number (GPIO numbering): ", font=labelFont)
+        self.servoPinLabel.place(x=50 + self.padding_x,y=65 + self.padding_y)
+
+        self.servoUpLabel = Label(tab, text="Pen up position, servo at (0-180): ", font=labelFont)
+        self.servoUpLabel.place(x=50 + self.padding_x,y=120 + self.padding_y)
+
+        self.servoDownLabel = Label(tab, text="Pen down position, servo at (0-180): ", font=labelFont)
+        self.servoDownLabel.place(x=50 + self.padding_x,y=175 + self.padding_y)
+
+        self.details = "-Raise and lower pen to check pen-up and pen-down positions"
+        self.detailsLabel = Label(tab, text=self.details, font=detailsTextFont)
+        self.detailsLabel.place(x=25 + self.padding_x, y=275 + self.padding_y)
+    
+    def add_entries(self, tab):
+        # Entries
+        self.servoPinEntry = Entry(tab, font=entryFieldFont)
+        self.servoPinEntry.insert(0, servoSettingsEntries[0])
+        self.servoPinEntry.place(x=400 + self.padding_x,y=67 + self.padding_y, width=46, height=30)
+
+        self.servoUpEntry = Entry(tab, font=entryFieldFont)
+        self.servoUpEntry.insert(0, servoSettingsEntries[1])
+        self.servoUpEntry.place(x=400 + self.padding_x,y=122 + self.padding_y, width=46, height=30)
+
+        self.servoDownEntry = Entry(tab, font=entryFieldFont)
+        self.servoDownEntry.insert(0, servoSettingsEntries[2])
+        self.servoDownEntry.place(x=400 + self.padding_x,y=177 + self.padding_y, width=46, height=30)
+    
+    def print_servo_entries(self):
+        print(f'Servo pin: {servoSettingsEntries[0]} Up position: {servoSettingsEntries[1]} Down position: {servoSettingsEntries[2]}')
+
+    def update(self):
+        # Get values from entries
+        entry_value1 = self.servoPinEntry.get()
+        entry_value2 = self.servoUpEntry.get()
+        entry_value3 = self.servoDownEntry.get()
+
+        # Save values globally
+        servoSettingsEntries[0] = str(entry_value1)
+        servoSettingsEntries[1] = str(entry_value2)
+        servoSettingsEntries[2] = str(entry_value3)
+
+        # Print
+        self.print_servo_entries()
+  
 
 def open_settings_window():
     # Define window configuration
@@ -69,24 +100,29 @@ def open_settings_window():
 
     # Define tab control widget & tabs
     tabControl = ttk.Notebook(window)
-    tab1 = ttk.Frame(tabControl)
-    tab2 = ttk.Frame(tabControl)
-    tab3 = ttk.Frame(tabControl)
+    tab1Frame = ttk.Frame(tabControl)
+    # tab2 = ttk.Frame(tabControl)
+    # tab3 = ttk.Frame(tabControl)
+
+    #servo_settings_tab(tab1)
+    firstTab = Servo_Settins_Tab(tab1Frame)
+    firstTab.print_servo_entries()
 
     # Add tabs to tab control widget
-    tabControl.add(tab1, text='Servo settings')
-    tabControl.add(tab2, text='Stepper 1 (egg) settings')    
-    tabControl.add(tab3, text='Stepper 2 (pen) settings')
+    tabControl.add(tab1Frame, text='Servo settings')
+    # tabControl.add(tab2, text='Stepper 1 (egg) settings')    
+    # tabControl.add(tab3, text='Stepper 2 (pen) settings')
+
     # Render
     tabControl.pack(expand=1,fill='both')
-
-    servo_settings_tab(tab1)
 
     # saveButton2 = Button(tab2, text="Save", font = saveButtonFont, width = 5, fg = 'black')
     # saveButton2.place(x=290,y=420)
 
     # saveButton3 = Button(tab3, text="Save", font = saveButtonFont, width = 5, fg = 'black')
     # saveButton3.place(x=290,y=420)
+    window.mainloop()
+
 
 
 def quit_program():
@@ -110,7 +146,5 @@ settingButton.place(x=310,y=320)
 
 quitButton = Button(gui, text="Quit", font = templateButtonFont, width=9, fg = quitTextColor, command=quit_program)
 quitButton.place(x=310,y=440)
-
-
 
 gui.mainloop()  
